ae77a437691074a6da4fa28682d376e417b022a1
SamuelCagnani <samuelcagnani@gmail.com>
2022-06-21 12:26:30 -0300

    confirmar

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const nav = document.querySelector('#header nav')
-    const icons = document.querySelectorAll('nav .toggle')
-    
-    
-    for(const element of icons){
-       element.addEventListener('click', function(){
-          nav.classList.toggle('show')
-       })
-    }
+const icons = document.querySelectorAll('nav .toggle')
+
+for (const element of icons) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+const links = document.querySelectorAll('nav ul li a')
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
